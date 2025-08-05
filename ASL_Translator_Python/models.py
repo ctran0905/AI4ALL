233ae2269f09ae0c297c240dadae2e475619d52f
James Boateng <jamesboateng@mac.lan>
2025-08-05 19:29:53 -0400

    Fix dependency issues

--- ASL_Translator_Python/models.py
+++ ASL_Translator_Python/models.py
@@ -8,7 +8,7 @@ NUM_CLASSES = 29  # Or however many signs you're classifying
 def load_model():
     model = models.resnet50(pretrained=False)
     model.fc = nn.Linear(model.fc.in_features, NUM_CLASSES)
-    model_path = os.path.join("models", "asl_model_epoch5.pth")
+    model_path = os.path.join("ASL_Translator_Python/models", "asl_model_epoch5.pth")
     model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
     model.eval()
     return model
